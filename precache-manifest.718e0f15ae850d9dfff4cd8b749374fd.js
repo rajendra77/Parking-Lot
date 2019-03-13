58573495b4b03df7e69791a2e9dea9aee0d280c5
rajendra77 <rs26105@gmail.com>
2019-03-13 22:54:35 +0530

    Updates

---
+++ precache-manifest.718e0f15ae850d9dfff4cd8b749374fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f894acb7018e67bdb6f",
+    "url": "/App/static/js/runtime~main.9bf40d8f.js"
+  },
+  {
+    "revision": "6a187ece75aee44aa3d9",
+    "url": "/App/static/js/main.c837fd78.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/App/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "6a187ece75aee44aa3d9",
+    "url": "/App/static/css/main.c67e186b.chunk.css"
+  },
+  {
+    "revision": "76d064497a96f77e21488768af5dc024",
+    "url": "/App/index.html"
+  }
+];
